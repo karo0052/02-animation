ed3d82fe5fe6f7ddfda6d41aa3554c676fc0a645
Karoline Lüthje <Karoline@Karolines-MacBook-Pro-4.local>
2018-04-13 13:39:27 +0200

    JS automatisk

--- animation.js
+++ animation.js
@@ -37,21 +37,19 @@ $(window).on("load", sidenVises);
 
 function sidenVises() {
     console.log("sidenVises");
-    // Start lyd: fuglefløjt
-    // Start lyd: støj
+    // Start lyd: fuglefløjt_støj
     // Start sprite-anim: .baby_blinke
     $("#baby_sprite").addClass("baby_blinke");
     // Vis sprite: .mor_armsdown
-    $("#mor_sprite").addClass("mor_armsdown");
 
-    $("#riskiks").addClass("buttons_none");
-    $("#rosiner").addClass("buttons_none");
+    // når der er gået 3,5 sek: babySigerHej
+    setTimeout(babySigerHej, 3500);
 
 }
 
-// når der er gået 3 sek: babySigerHej
+
 //knap til sømbræt
-$("#hej").on("click", babySigerHej);
+//$("#hej").on("click", babySigerHej);
 
 function babySigerHej() {
     console.log("babySigerHej");
@@ -61,14 +59,20 @@ function babySigerHej() {
     // Start sprite-anim: .baby_vinke
     $("#baby_sprite").addClass("baby_vinke");
     // Spil lyd: hej
+
+    // når sprite-anim: .baby_vinker er færdig: maageGaarModVenstre
+    $("#baby_sprite").on("animationend", maageGaarModVenstre);
 }
 
-// når sprite-anim: .baby_vinker er færdig: maageGaarModVenstre
 //knap til sømbræt
-$("#maage_left").on("click", maageGaarModVenstre);
+//$("#maage_left").on("click", maageGaarModVenstre);
+
+
 
 function maageGaarModVenstre() {
     console.log("maageGaarModVenstre");
+
+    $("#baby_sprite").off("animationend", maageGaarModVenstre);
 
     $("#baby_sprite").removeClass("baby_vinke");
     // Vis sprite: .baby
@@ -77,13 +81,18 @@ function maageGaarModVenstre() {
     $("#maage_container").addClass("maage_move_left");
     // Start sprite-anim: .maage_walkcycle_left
     $("#maage_sprite").addClass("maage_walkcycle_left");
+
+    // når flytte-anim: .maage_move_left er færdig: maageKigger
+    $("#maage_container").on("animationend", maageKigger);
 }
-// når flytte-anim: .maage_move_left er færdig: maageKigger
+
 // knap til sømbræt
-$("#maage_kigger").on("click", maageKigger);
+//$("#maage_kigger").on("click", maageKigger);
 
 function maageKigger() {
     console.log("maageKigger");
+
+    $("#maage_container").off("animationend", maageKigger);
 
     $("#maage_container").removeClass("maage_move_left");
     // Stop sprite-anim: .maage_walkcycle_left
@@ -92,10 +101,13 @@ function maageKigger() {
     $("#maage_sprite").addClass("maage_kigge");
     // Sæt position container: .maage_position_in
     $("#maage_container").addClass("maage_position_in");
+
+    // når der er gået 2 sek: maageGaarModHoejre
+    setTimeout(maageGaarModHoejre, 2000);
 }
-// når der er gået 2 sek: maageGaarModHoejre
+
 // knap til sømbræt
-$("#maage_right").on("click", maageGaarModHoejre);
+//$("#maage_right").on("click", maageGaarModHoejre);
 
 function maageGaarModHoejre() {
     console.log("maageGaarModHoejre");
@@ -109,13 +121,17 @@ function maageGaarModHoejre() {
     // Start flytte-anim: .maage_move_right
     $("#maage_container").addClass("maage_move_right");
 
+    // når flytte-anim: .maage_move_right er færdig: babyTigger
+    $("#maage_container").on("animationend", babyTigger);
 }
-// når flytte-anim: .maage_move_right er færdig: babyTigger
+
 // knap til sømbræt
-$("#baby_tigger").on("click", babyTigger);
+//$("#baby_tigger").on("click", babyTigger);
 
 function babyTigger() {
     console.log("babyTigger");
+
+    $("#maage_container").off("animationend", babyTigger);
 
     $("#baby_sprite").removeClass(".baby");
     $("#maage_container").removeClass("maage_move_right");
@@ -124,20 +140,28 @@ function babyTigger() {
     // Start sprite-anim: .baby_tigge
     $("#baby_sprite").addClass("baby_tigge");
     // Start lyd: eheheh
+
+    // når sprite-anim: .baby_tigge er færdig: babyLaverDaadyroejne
+    $("#baby_sprite").on("animationend", babyLaverDaadyroejne);
 }
-// når sprite-anim: .baby_tigge er færdig: babyLaverDaadyroejne
+
 // knap til sømbræt
-$("#baby_daadyr").on("click", babyLaverDaadyroejne);
+//$("#baby_daadyr").on("click", babyLaverDaadyroejne);
 
 function babyLaverDaadyroejne() {
     console.log("babyLaverDaadyroejne");
+
+    $("#baby_sprite").off("animationend", babyLaverDaadyroejne);
 
     $("#baby_sprite").removeClass("baby_tigge");
     // Stop lyd: eheheh
     // Start sprite-anim: .baby_daadyr
     $("#baby_sprite").addClass("baby_daadyr");
+
+    // når der er gået 2 sek: morBevaegerArme
+    setTimeout(morBevaegerArme, 2000);
 }
-// når der er gået 2 sek: morBevaegerArme
+
 //knap til sømbræt
 $("#mor_arme").on("click", morBevaegerArme);
 
@@ -152,29 +176,37 @@ function morBevaegerArme() {
     $("#baby_sprite").removeClass("baby_daadyr");
     // Start sprite-anim: .mor_arme
     $("#mor_sprite").addClass("mor_arme");
+
+    // når sprite-anim: .mor_arme er færdig: viseKnapper
+    $("#mor_sprite").on("animationend", viseKnapper);
 }
-// når sprite-anim: .mor_arme er færdig: viseKnapper
+
 // knap til sømbræt
-$("#vise_knapper").on("click", viseKnapper);
+//$("#vise_knapper").on("click", viseKnapper);
 
 function viseKnapper() {
     console.log("viseKnapper");
+    $("#mor_sprite").off("animationend", viseKnapper);
 
     $("#mor_sprite").removeClass("mor_arme");
     // Vis sprite: .mor
     $("#mor_sprite").addClass("mor_armsup");
-    // Stop lyd: fuglefløjt
-    // Stop lyd: støj
+    // Stop lyd: fuglefløjt_støj
     // Spil lyd: riskiks_eller_rosiner
-    // Start anim?: .buttons
     $("#riskiks").removeClass("buttons_none");
     $("#rosiner").removeClass("buttons_none");
 
+    $("#riskiks").addClass("buttons_pulse");
+    $("#rosiner").addClass("buttons_pulse");
+
     /*Her skal knapperne riskiks og rosin vises, hvorefter brugeren tager et valg - forgreningstrin. Lige nu vælger brugeren riskiks*/
+
+    // når brugeren har klikket på riskiksen: babySpiserRiskiks
+    $("#riskiks").on("click", babySpiserRiskiks);
 }
-// når brugeren har klikket på riskiksen: babySpiserRiskiks
+
 //knap til sømbræt
-$("#spise_riskiks").on("click", babySpiserRiskiks);
+//$("#spise_riskiks").on("click", babySpiserRiskiks);
 
 function babySpiserRiskiks() {
     console.log("babySpiserRiskiks");
@@ -183,13 +215,18 @@ function babySpiserRiskiks() {
     $("#mor_sprite").removeClass("mor_armsup");
     $("#mor_sprite").addClass("mor_armsdown");
 
-    // Stop anim?: .buttons
-    // Start lyd: fuglefløjt
-    // Start lyd: støj
+    // Start lyd: fuglefløjt_støj
     // Start sprite-anim: .baby_spiseriskiks
     $("#baby_sprite").addClass("baby_spiseriskiks");
+
+    // når sprite-anim: .baby_spiseriskiks er færdig: babyVinkerFarvel
+    $("#baby_sprite").on("animationend", babyVinkerFarvel);
+
+    // når der er gået 1.5 sek: riskiksLyd
+    setTimeout(riskiksLyd, 1500);
 }
-// når der er gået 1.5 sek: riskiksLyd
+
+
 // knap til sømbræt
 $("#riskiks_lyd").on("click", riskiksLyd);
 
@@ -199,7 +236,7 @@ function riskiksLyd() {
     // Start lyd: riskiks
 }
 
-// når sprite-anim: .baby_spiseriskiks er færdig: babyVinkerFarvel
+
 // knap til sømbræt
 $("#farvel").on("click", babyVinkerFarvel);
 
